b780815e85416a96e0cf2934c113c273a6aeb4f2
Takumi Akiyama <t.akiym@gmail.com>
2013-08-01 17:35:40 +0900

    go fmt

--- README.md
+++ README.md
@@ -9,49 +9,49 @@ Lightweight skkserv implementation for golang
     package main
 
     import (
-        "code.google.com/p/mahonia"
-        "encoding/json"
-        "github.com/akiym/go-skkserv"
-        "net/http"
-        "net/url"
+    	"code.google.com/p/mahonia"
+    	"encoding/json"
+    	"github.com/akiym/go-skkserv"
+    	"net/http"
+    	"net/url"
     )
 
-    type GoogleIMESKK struct {}
+    type GoogleIMESKK struct{}
 
     func (s *GoogleIMESKK) Request(text string) ([]string, error) {
-        words, err := Transliterate(text)
-        if err != nil {
-            return nil, err
-        }
-        return words, nil
+    	words, err := Transliterate(text)
+    	if err != nil {
+    		return nil, err
+    	}
+    	return words, nil
     }
 
     var enc = mahonia.NewEncoder("euc-jp")
 
     func Transliterate(text string) (words []string, err error) {
-        text = enc.ConvertString(text)
-        v := url.Values{"langpair": {"ja-Hira|ja"}, "text": {text + ","}}
-        resp, err := http.Get("http://www.google.com/transliterate?" + v.Encode())
-        if err != nil {
-            return nil, err
-        }
-        defer resp.Body.Close()
-        dec := json.NewDecoder(resp.Body)
-        var w [][]interface{}
-        if err := dec.Decode(&w); err != nil {
-            return nil, err
-        }
-        for _, v := range w[0][1].([]interface{}) {
-            word := v.(string)
-            result, ok := enc.ConvertStringOK(word)
-            if ok {
-                words = append(words, result)
-            }
-        }
-        return words, nil
+    	text = enc.ConvertString(text)
+    	v := url.Values{"langpair": {"ja-Hira|ja"}, "text": {text + ","}}
+    	resp, err := http.Get("http://www.google.com/transliterate?" + v.Encode())
+    	if err != nil {
+    		return nil, err
+    	}
+    	defer resp.Body.Close()
+    	dec := json.NewDecoder(resp.Body)
+    	var w [][]interface{}
+    	if err := dec.Decode(&w); err != nil {
+    		return nil, err
+    	}
+    	for _, v := range w[0][1].([]interface{}) {
+    		word := v.(string)
+    		result, ok := enc.ConvertStringOK(word)
+    		if ok {
+    			words = append(words, result)
+    		}
+    	}
+    	return words, nil
     }
 
     func main() {
-        var server = skkserv.NewServer(":55100", &GoogleIMESKK{})
-        server.Run()
+    	var server = skkserv.NewServer(":55100", &GoogleIMESKK{})
+    	server.Run()
     }
--- skkserv.go
+++ skkserv.go
@@ -1,106 +1,106 @@
 package skkserv
 
 import (
-    "bufio"
-    "log"
-    "net"
-    "strings"
+	"bufio"
+	"log"
+	"net"
+	"strings"
 )
 
 const SkkServVersion = "0.0.1"
 
 type Handler interface {
-    Request(text string) ([]string, error)
+	Request(text string) ([]string, error)
 }
 
 type SKKServ struct {
-    Port string
-    Handler Handler
+	Port    string
+	Handler Handler
 }
 
 func NewServer(port string, handler Handler) *SKKServ {
-    server := &SKKServ{
-        Port: port,
-        Handler: handler,
-    }
-    return server
+	server := &SKKServ{
+		Port:    port,
+		Handler: handler,
+	}
+	return server
 }
 
 func (s *SKKServ) Run() {
-    ln, err := net.Listen("tcp", s.Port)
-    if err != nil {
-        log.Fatal(err)
-    }
-    for {
-        conn, err := ln.Accept()
-        if err != nil {
-            continue
-        }
-        go s.handleConnection(conn)
-    }
+	ln, err := net.Listen("tcp", s.Port)
+	if err != nil {
+		log.Fatal(err)
+	}
+	for {
+		conn, err := ln.Accept()
+		if err != nil {
+			continue
+		}
+		go s.handleConnection(conn)
+	}
 }
 
 func (s *SKKServ) handleConnection(conn net.Conn) {
-    defer conn.Close()
-    reader := bufio.NewReader(conn)
-    for {
-        c, err := reader.ReadByte()
-        if err != nil {
-            return
-        }
-        switch c {
-        case '0':
-            return // end of connection
-        case '1':
-            buf, err := reader.ReadBytes(' ')
-            if err != nil {
-                return
-            }
-            s.serverRequest(conn, buf)
-        case '2':
-            s.serverVersion(conn)
-        case '3':
-            s.serverHost(conn)
-        }
-    }
+	defer conn.Close()
+	reader := bufio.NewReader(conn)
+	for {
+		c, err := reader.ReadByte()
+		if err != nil {
+			return
+		}
+		switch c {
+		case '0':
+			return // end of connection
+		case '1':
+			buf, err := reader.ReadBytes(' ')
+			if err != nil {
+				return
+			}
+			s.serverRequest(conn, buf)
+		case '2':
+			s.serverVersion(conn)
+		case '3':
+			s.serverHost(conn)
+		}
+	}
 }
 
 // "1eee " eee is keyword in EUC code with ' ' at the end
 func (s *SKKServ) serverRequest(conn net.Conn, buf []byte) error {
-        var resp string
-        text := string(buf[:len(buf)-1])
-        words, err := s.Handler.Request(text)
-        if err != nil {
-            // server error
-            resp = "0"
-        } else {
-            if len(words) > 0 {
-                // found
-                resp = "1/" + strings.Join(words, "/") + "/"
-            } else {
-                // not found
-                resp = "4" + text
-            }
-        }
-        if _, err := conn.Write([]byte(resp + "\n")); err != nil {
-            return err
-        }
-        return nil
+	var resp string
+	text := string(buf[:len(buf)-1])
+	words, err := s.Handler.Request(text)
+	if err != nil {
+		// server error
+		resp = "0"
+	} else {
+		if len(words) > 0 {
+			// found
+			resp = "1/" + strings.Join(words, "/") + "/"
+		} else {
+			// not found
+			resp = "4" + text
+		}
+	}
+	if _, err := conn.Write([]byte(resp + "\n")); err != nil {
+		return err
+	}
+	return nil
 }
 
 // "2" skkserv version number
 func (s *SKKServ) serverVersion(conn net.Conn) error {
-    if _, err := conn.Write([]byte(SkkServVersion + " ")); err != nil {
-        return err
-    }
-    return nil
+	if _, err := conn.Write([]byte(SkkServVersion + " ")); err != nil {
+		return err
+	}
+	return nil
 }
 
 // "3" hostname and its IP addresses
 func (s *SKKServ) serverHost(conn net.Conn) error {
-    addr := conn.LocalAddr().String()
-    if _, err := conn.Write([]byte(addr + " ")); err != nil {
-        return err
-    }
-    return nil
+	addr := conn.LocalAddr().String()
+	if _, err := conn.Write([]byte(addr + " ")); err != nil {
+		return err
+	}
+	return nil
 }
